c56e84c08f75b8adc65d2d84e20bfcd9a8e458a8
Prins Ocampo <85613393+Gambit42@users.noreply.github.com>
2025-03-02 19:46:25 +0800

    add for loop

---
+++ for_loops/index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="/for_loops/index.php" method="post">
+        <!-- <label>
+            Number to count to:
+</label>
+    <input name="numToLoop"/> -->
+        <label>
+            Number to from:
+</label>
+    <input name="numToLoopBackwards"/>
+    <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
+
+
+
+<?php
+
+$message = "Sta!";
+// $numToLoop = $_POST['numToLoop'];
+$numToLoopBackwards = $_POST['numToLoopBackwards'];
+
+//first is index always start with 0
+// for($i = 1; $i <= $numToLoop; $i++){
+//     echo "$message index is $i <br/>";
+// }
+
+echo "$numToLoopBackwards";
+
+for($i = $numToLoopBackwards; $i > 0; $i--){
+    echo "$i <br/>";
+}
+
+//1. Declare variable
+//2. Check if condition is true.
+//3. If condition true, continue the loop
+
+?>
